642204cdcc2384797b1307c476e65a868694198f
immanuel nowpert <nowpertimmanuel@gmail.com>
2023-10-31 15:19:07 +0530

    ADD: added list

--- HarryPotter/ListCharacters/ListCharacters.swift
+++ HarryPotter/ListCharacters/ListCharacters.swift
@@ -9,14 +9,34 @@ import SwiftUI
 import ComposableArchitecture
 
 struct ListCharactersView: View {
-    @State private var characters : [Character] = []
     let store : StoreOf<ListCharactersFeature>
     var body: some View {
-        WithViewStore(self.store, observe: {$0}){viewStore in 
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .onAppear(){
-                    viewStore.send(.getCharacters)
-                }
+        
+        WithViewStore(self.store, observe: {$0}){viewStore in
+            
+            ScrollView(.vertical) {
+                LazyVGrid(columns: [GridItem(spacing:0),GridItem(spacing:0)],spacing: 0, content: {
+                    ForEach(viewStore.characters){character in
+                        if let url = character.image  , !url.isEmpty {
+                            AsyncImage(url: URL(string: url)){
+                                Image in
+                                Image
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width:200,height: 300)
+                                    
+                            }placeholder: {
+                                ProgressView()
+                            }
+                            .onTapGesture {
+                                // TODO :  ACTION TO ADD TO PATH
+                            }
+                        }
+                    }
+                }).task{
+                        viewStore.send(.getCharacters)
+            }
+            }
         }
     }
 }
